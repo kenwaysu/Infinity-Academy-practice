fee4691ae255b757800a3dac9e975e931f2685ad
suboeken <suboeken@gmail.com>
2024-06-01 10:09:45 +0800

    add DOM practice

--- web.js
+++ web.js
@@ -77,13 +77,41 @@ function resetCart(){
     cart.product.totalPrice = 0;
     let jsonCart = JSON.stringify(cart)
     localStorage.setItem("cart",jsonCart)
-    // return cart
 }
 
 // function subscripe(){
 //     const subscripeButton = document.querySelector('#subscripe')
 //     subscripeButton.innerHTML = "已訂閱"
 // }
+const totalPrice = document.querySelector("#totalPrice")
+totalPrice.innerHTML = `totalPrice : ${cart.product.totalPrice  }`
+
+document.addEventListener('DOMContentLoaded', () =>{
+    const product_1 = document.querySelector('#product-1')
+    product_1.innerHTML = `product 1 : ${cart.product.product_1}`
+    product_1 = document.addEventListener("click",() => {
+        product_1.innerHTML = `product 1 : ${cart.product.product_1}`
+        totalPrice.innerHTML = `totalPrice : ${cart.product.totalPrice  }`
+    })
+})
+
+document.addEventListener('DOMContentLoaded', () =>{
+    const product_2 = document.querySelector('#product-2')
+    product_2.innerHTML = `product 2 : ${cart.product.product_2}`
+    product_2 = document.addEventListener("click",() => {
+        product_2.innerHTML = `product 2 : ${cart.product.product_2}`
+        totalPrice.innerHTML = `totalPrice : ${cart.product.totalPrice  }`
+    })
+})
+
+document.addEventListener('DOMContentLoaded', () =>{
+    const product_3 = document.querySelector('#product-3')
+    product_3.innerHTML = `product 3 : ${cart.product.product_3}`
+    product_3 = document.addEventListener("click",() => {
+        product_3.innerHTML = `product 3 : ${cart.product.product_3}`
+        totalPrice.innerHTML = `totalPrice : ${cart.product.totalPrice  }`
+    })
+})
 
 document.addEventListener('DOMContentLoaded', () => {
     const button = document.querySelector('#subscripe');
